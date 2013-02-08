cdf033fe33d0e2c238cc083cd42995416db0e5cb
Jason Harmon <eggman891@yahoo.co.uk>
2013-02-08 07:15:51 +0000

    Github misbehaving. Having to commit indvidually

--- core/src/main/java/com/ronma/awesomecalc/core/loadout/FroggyTest.java
+++ core/src/main/java/com/ronma/awesomecalc/core/loadout/FroggyTest.java
@@ -12,7 +12,17 @@ public class FroggyTest {
             "spr_froggyG_splashDash_hammerPants.png",
             "spr_froggyG_splashDash_clockNecklace.png",
             "spr_froggyG_splashDash_hydroSmash.png"
-        };;    
+        };
+    private String [] _ability2 = new String[] {
+            "spr_froggyG_tornado.png",
+            "spr_froggyG_tornado_boomBox.png",
+            "spr_froggyG_tornado_turboTape.png",
+            "spr_froggyG_tornado_cantTouchThis.png",
+            "spr_froggyG_tornado_bioFuelCells.png",
+            "spr_froggyG_tornado_twistedTweeters.png",
+            "spr_froggyG_tornado_rightBackAtYa.png"
+        }; 
+    
     private String [] _autoAttack = new String[] {
             "spr_froggyG_autoAttack.png",
             "spr_froggyG_autoAttack_swirlingOctopusCartridges.png",
@@ -23,14 +33,36 @@ public class FroggyTest {
             "spr_froggyG_autoAttack_thornFish.png"
         };
     
+    private String [] _utility = new String[] {
+            "spr_froggyG_utility.png",
+            "spr_froggyG_utility_powerPillsLight.png",
+            "spr_froggyG_utility_medICan.png",
+            "spr_froggyG_utility_spaceAirMax.png",
+            "spr_froggyG_utility_solarTree.png",
+            "spr_froggyG_utility_piggyBank.png",
+            "spr_froggyG_utility_powerPills.png"
+        }; 
+    private String _fullPortrait = new String("spr_froggyG_fullPortrait.png");
+    
     public FroggyTest() {
         res = new NautResources();        
     }
     
     public void LoadResources() {
+        res.SetPortrait(Global.m_assetManager.getImage("images/" + _fullPortrait));
         for (int i = 0; i < 7; i ++) {
-            res.AddAbility1Slot(Global.m_assetManager.getImage("images/" + _ability1[i]));
-            res.AddAutoAttackSlot(Global.m_assetManager.getImage("images/" + _autoAttack[i]));
+            if (i == 0) {
+                res.SetAbility1Icon(Global.m_assetManager.getImage("images/" + _ability1[i]));
+                res.SetAbility2Icon(Global.m_assetManager.getImage("images/" + _ability2[i]));
+                res.SetAutoAttackIcon(Global.m_assetManager.getImage("images/" + _autoAttack[i]));
+                res.SetUtilityIcon(Global.m_assetManager.getImage("images/" + _utility[i]));
+            }
+            else {
+                res.AddAbility1Slot(Global.m_assetManager.getImage("images/" + _ability1[i]));
+                res.AddAbility2Slot(Global.m_assetManager.getImage("images/" + _ability2[i]));                
+                res.AddAutoAttackSlot(Global.m_assetManager.getImage("images/" + _autoAttack[i]));
+                res.AddUtilitySlot(Global.m_assetManager.getImage("images/" + _utility[i]));
+            }
         }
     }
 }
--- core/src/main/java/com/ronma/awesomecalc/core/loadout/NautResources.java
+++ core/src/main/java/com/ronma/awesomecalc/core/loadout/NautResources.java
@@ -2,13 +2,17 @@ package com.ronma.awesomecalc.core.loadout;
 
 import playn.core.Image;
 import java.util.ArrayList;
-import playn.core.AssetWatcher;
+import com.ronma.awesomecalc.core.loadout.LoadoutRow;
 
 public class NautResources {
-    public final int NumSlotsPerAbility = 6;
+    public static final int NumSlotsPerAbility = 6;
+    protected Image _ability1Icon;
     protected ArrayList<Image> _ability1;
+    protected Image _ability2Icon;
     protected ArrayList<Image> _ability2;
+    protected Image _autoAttackIcon;
     protected ArrayList<Image> _autoAttack;
+    protected Image _utilityIcon;
     protected ArrayList<Image> _utility;
     protected Image _portraitIcon;
     protected Image _portraitFull;
@@ -23,6 +27,59 @@ public class NautResources {
     
     public void SetPortraitIcon(Image img) {
         _portraitIcon = img;
+    }
+    
+    public void SetIcon(LoadoutRowType type, Image img) {
+        switch (type) {
+            case ABILITY1:
+                SetAbility1Icon(img);
+            break;
+
+            case ABILITY2:
+                SetAbility2Icon(img);
+            break;
+
+            case AUTO_ATTACK:
+                SetAutoAttackIcon(img);
+            break;
+
+            case UTILITY:
+                SetUtilityIcon(img);
+            break;
+        }
+    }
+    
+    public void SetAbility1Icon(Image img) {
+        _ability1Icon = img;
+    }
+    public void SetAbility2Icon(Image img) {
+        _ability2Icon = img;
+    }
+    public void SetAutoAttackIcon(Image img) {
+        _autoAttackIcon = img;
+    }
+    public void SetUtilityIcon(Image img) {
+        _utilityIcon = img;
+    }
+    
+    public void AddSlot(LoadoutRowType type, Image img) {
+        switch (type) {
+            case ABILITY1:
+                AddAbility1Slot(img);
+            break;
+                
+            case ABILITY2:
+                AddAbility2Slot(img);
+            break;
+                
+            case AUTO_ATTACK:
+                AddAutoAttackSlot(img);
+            break;
+                
+            case UTILITY:
+                AddUtilitySlot(img);
+            break;
+        }
     }
     
     public void AddAbility1Slot(Image img) {
@@ -59,6 +116,56 @@ public class NautResources {
     
     public Image GetPortraitIcon() {
         return _portraitIcon;
+    }
+    
+    public Image GetAbilityIcon(LoadoutRowType type) {
+        switch (type) {
+            case ABILITY1:
+                return GetAbility1Icon();
+                
+            case ABILITY2:
+                return GetAbility2Icon();
+                
+            case AUTO_ATTACK:
+                return GetAutoAttackIcon();
+                
+            case UTILITY: 
+                return GetUtilityIcon();
+        }
+        
+        return null;
+    }
+        
+    
+    public Image GetAbility1Icon() {
+        return _ability1Icon;
+    }
+    public Image GetAbility2Icon() {
+        return _ability2Icon;
+    }
+    public Image GetAutoAttackIcon() {
+        return _autoAttackIcon;
+    }
+    public Image GetUtilityIcon() {
+        return _utilityIcon;
+    }
+    
+    public Image GetSlot(LoadoutRowType type, int col) {
+        switch (type) {
+            case ABILITY1:
+                return GetAbility1Slot(col);
+                
+            case ABILITY2:
+                return GetAbility2Slot(col);
+                
+            case AUTO_ATTACK:
+                return GetAutoAttackSlot(col);
+                
+            case UTILITY: 
+                return GetUtilitySlot(col);
+        }
+        
+        return null;
     }
     
     public Image GetAbility1Slot(int col) {
@@ -108,10 +215,16 @@ public class NautResources {
     public ArrayList<Image> GetFullResourceList() {
         ArrayList<Image> fullList = new ArrayList<Image>();
         
+        if (_ability1Icon != null) fullList.add(_ability1Icon);
+        if (_ability2Icon != null) fullList.add(_ability2Icon);
+        if (_autoAttackIcon != null) fullList.add(_autoAttackIcon);
+        if (_utilityIcon != null) fullList.add(_utilityIcon);
+        
         if (_ability1 != null)  fullList.addAll(_ability1);
         if (_ability2 != null)  fullList.addAll(_ability2);
         if (_autoAttack != null)fullList.addAll(_autoAttack);        
         if (_utility != null) fullList.addAll(_utility);
+        
         if (_portraitFull != null) fullList.add(_portraitFull);
         if (_portraitIcon != null) fullList.add(_portraitIcon);
         
